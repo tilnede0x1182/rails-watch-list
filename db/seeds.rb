d69e646172e6f6eeb0fe194debc707a21970fd0f
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-02-27 12:58:32 +0100

    Création des models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Database cleaned!"
