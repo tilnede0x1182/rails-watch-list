98dbf368b2a0780e5a6a5b663564b312c4712116
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-03-02 16:51:08 +0100

    Exercice rails-watch-list completed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list), notice: "Le film a bien été ajouté à la liste !"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other, notice: "Le signet a été supprimé."
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,53 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-puts "Cleaning database..."
 
+# Suppression des données existantes pour éviter les doublons
+puts "Suppression des anciens records..."
 Bookmark.destroy_all
 List.destroy_all
 Movie.destroy_all
 
-puts "Database cleaned!"
+# Création des films (comme dans l'énoncé)
+puts "Création des films..."
+movies = [
+  { title: "Wonder Woman 1984", overview: "Wonder Woman entre en conflit avec l'Union soviétique pendant la Guerre Froide.", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9 },
+  { title: "The Shawshank Redemption", overview: "Un banquier est condamné à tort pour un double meurtre.", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7 },
+  { title: "Titanic", overview: "L'histoire du Titanic racontée par une survivante âgée de 101 ans.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9 },
+  { title: "Ocean's Eight", overview: "Une équipe de voleuses prépare le casse du siècle.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0 }
+]
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
+puts "Films créés !"
+
+# Création des listes
+puts "Création des listes..."
+lists = [
+  { name: "Action" },
+  { name: "Drama" },
+  { name: "Science-Fiction" },
+  { name: "Gril Power" },
+  { name: "All taime favourites" }
+]
+
+lists.each do |list|
+  List.create!(list)
+end
+puts "Listes créées !"
+
+# Ajout automatique de quelques bookmarks pour tester
+puts "Ajout de signets..."
+lists = List.all
+movies = Movie.all
+
+lists.each do |list|
+  sample_movies = movies.sample(2) # Choisir 2 films au hasard par liste
+  sample_movies.each do |movie|
+    Bookmark.create!(list: list, movie: movie, comment: "Super film à voir !")
+  end
+end
+puts "Signets ajoutés !"
+
+puts "Seed terminée !"
